5e6020d904c6a8e0a18c33ca2fedb8a969bddb86
NIKOLLBIBAJ Jeton <jeton.nikollbibaj@vinci-energies.net>
2025-04-03 15:33:50 +0200

    Finish the UI of add car modal popup, the upload image function, inputs selects

--- src/app/pages/modal/modal.component.ts
+++ src/app/pages/modal/modal.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-modal',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
+
 })
 export class ModalComponent {
 
   @ViewChild('modal') modal!: ElementRef;
+
+  fileData: File | null = null;
+selectedFile: any;
+
+fileName: string = '';
+filePreview: string | ArrayBuffer | null = null;
+
+ngOnInit() {
+  console.log('test',this.fileData)
+}
 
   openModal() {
     this.modal.nativeElement.style.display = 'block';
@@ -18,4 +31,29 @@ export class ModalComponent {
     this.modal.nativeElement.style.display = 'none';
   }
 
+  onFileSelected(event: any) {
+    const input = event.target as HTMLInputElement;
+   
+    this.fileData = event.target.files[0];
+
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      this.fileName = file.name;
+
+      // Create a file preview
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.filePreview = reader.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+
+
+
+
+
+  
 }
